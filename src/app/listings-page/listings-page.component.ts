791d0627b5edd67433accaa12f7b8a8eae52520e
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-04 17:38:06 +0530

    Added fake data to the listings-paage

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listings-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './listings-page.component.html',
-  styleUrl: './listings-page.component.css'
+  styleUrl: './listings-page.component.css',
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 }
