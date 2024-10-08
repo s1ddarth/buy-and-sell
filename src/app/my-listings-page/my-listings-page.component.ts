abc11842465326e0009c2b0e7c3e5e885379aae2
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-08 17:58:33 +0530

    Added frontend functionality to My Listings page

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-my-listings-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './my-listings-page.component.html',
   styleUrl: './my-listings-page.component.css'
 })
 export class MyListingsPageComponent {
+  listings: Listing[] | undefined;
 
+  constructor() {}
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    // TODO: Implement with backend
+    alert(`Listing ${listingId} deleted`);
+  }
 }
