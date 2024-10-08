d458f122c12cb0037ab7c1d364456b68dd18d32c
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-08 16:33:33 +0530

    Completed contact seller page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './contact-page.component.html',
-  styleUrl: './contact-page.component.css'
+  styleUrl: './contact-page.component.css',
 })
 export class ContactPageComponent {
+  email: string = '';
+  message: string = '';
+  listing: Listing | undefined;
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
+    this.message = `Hi, I'm interested in your ${this.listing?.name.toLowerCase()}`;
+  }
+
+  sendMessage(): void {
+    alert('Message sent!');
+    this.router.navigateByUrl('/listings');
+  }
 }
