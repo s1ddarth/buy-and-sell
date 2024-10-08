ae6d0730e9ffeb0c2cd1756e774ef084bcbc6e39
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-08 19:19:25 +0530

    Edited edit-listing and new-listing page to use the listings data form

--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ListingDataFormComponent } from '../listing-data-form/listing-data-form.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-edit-listing-page',
   standalone: true,
-  imports: [],
+  imports: [ListingDataFormComponent],
   templateUrl: './edit-listing-page.component.html',
-  styleUrl: './edit-listing-page.component.css'
+  styleUrl: './edit-listing-page.component.css',
 })
 export class EditListingPageComponent {
+  listing: Listing | undefined;
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeMyListings.find((listing) => listing.id === id);
+  }
+  onSubmit(): void {
+    alert(`Saving changes...`);
+    this.router.navigateByUrl('/my-listings');
+  }
 }
--- src/app/new-listings-page/new-listings-page.component.ts
+++ src/app/new-listings-page/new-listings-page.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ListingDataFormComponent } from "../listing-data-form/listing-data-form.component";
 
 @Component({
   selector: 'app-new-listings-page',
   standalone: true,
   imports: [
-    FormsModule
-  ],
+    FormsModule,
+    ListingDataFormComponent
+],
   templateUrl: './new-listings-page.component.html',
   styleUrl: './new-listings-page.component.css',
 })
 export class NewListingsPageComponent {
-  name: string = '';
-  description: string = '';
-  price: string = '';
 
   constructor(private router: Router) {}
   ngOnInit(): void {}
