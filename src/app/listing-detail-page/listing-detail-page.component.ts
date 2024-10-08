be23321919cf0ea62467f366c77c9a722d4638a0
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-08 15:55:55 +0530

    Added buttons and display data to listing-detail page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './listing-detail-page.component.html',
-  styleUrl: './listing-detail-page.component.css'
+  styleUrl: './listing-detail-page.component.css',
 })
 export class ListingDetailPageComponent {
+  listing: Listing | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id===id)
+  }
 }
