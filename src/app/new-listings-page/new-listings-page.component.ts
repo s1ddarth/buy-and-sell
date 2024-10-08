73a8a34faaca3ae71d671a600499bb586da75ed7
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-08 18:14:55 +0530

    Added frontend functionality for new listings page

--- src/app/new-listings-page/new-listings-page.component.ts
+++ src/app/new-listings-page/new-listings-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-listings-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './new-listings-page.component.html',
-  styleUrl: './new-listings-page.component.css'
+  styleUrl: './new-listings-page.component.css',
 })
 export class NewListingsPageComponent {
+  name: string = '';
+  description: string = '';
+  price: string = '';
 
+  constructor(private router: Router) {}
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // TODO: Implement backend
+    alert(`New listing created`);
+    this.router.navigateByUrl('/my-listings');
+  }
 }
