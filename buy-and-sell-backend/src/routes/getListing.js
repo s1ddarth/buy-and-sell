814f0e20ac3e883b09abaa64fcf67b393d3ab37e
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-09 20:07:38 +0530

    Added @hapi/boom package for API error handling

--- buy-and-sell-backend/src/routes/getListing.js
+++ buy-and-sell-backend/src/routes/getListing.js
@@ -1,10 +1,14 @@
+import Boom from "@hapi/boom";
 import { fakeListings } from "./fake-data";
 
 export const getListingRoute = {
-  method: 'GET',
-  path: '/api/listings/{id}',
+  method: "GET",
+  path: "/api/listings/{id}",
   handler: (req, h) => {
     const id = req.params.id;
-    return fakeListings.find((listing) => listing.id === id);
+    const listing = fakeListings.find((listing) => listing.id === id);
+
+    if (!listing) throw Boom.notFound(`Listing ${id} does not exist`);
+    return listing;
   },
 };
