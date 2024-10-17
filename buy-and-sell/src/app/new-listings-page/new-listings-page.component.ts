ee45490cb9af6ce4b67a8bd43f73ba9fc532fb6e
Siddarth Menon <33069694+s1ddarth@users.noreply.github.com>
2024-10-17 17:41:00 +0530

    Updated listingsService and new listings component to connect to db

--- buy-and-sell/src/app/new-listings-page/new-listings-page.component.ts
+++ buy-and-sell/src/app/new-listings-page/new-listings-page.component.ts
@@ -1,26 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ListingDataFormComponent } from "../listing-data-form/listing-data-form.component";
+import { ListingDataFormComponent } from '../listing-data-form/listing-data-form.component';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-new-listings-page',
   standalone: true,
-  imports: [
-    FormsModule,
-    ListingDataFormComponent
-],
+  imports: [FormsModule, ListingDataFormComponent],
   templateUrl: './new-listings-page.component.html',
   styleUrl: './new-listings-page.component.css',
 })
 export class NewListingsPageComponent {
-
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private listingService: ListingsService
+  ) {}
   ngOnInit(): void {}
 
-  onSubmit(): void {
-    // TODO: Implement backend
-    alert(`New listing created`);
-    this.router.navigateByUrl('/my-listings');
+  onSubmit({ name, description, price }: { name: string; description: string; price: number }): void {
+    this.listingService
+      .createListing(name, description, price)
+      .subscribe(() => {
+        alert(`New listing created`);
+        this.router.navigateByUrl('/my-listings');
+      });
   }
+  
 }
